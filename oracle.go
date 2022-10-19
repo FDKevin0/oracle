93ecc58031a74d6431eba27959cfa6cf3e296b1d
FDKevin <fdkevinzero@gmail.com>
2022-10-19 10:10:22 +0800

    some fixup

--- oracle.go
+++ oracle.go
@@ -98,14 +98,19 @@ func (d Dialector) RewriteLimit(c clause.Clause, builder clause.Builder) {
 			}
 		}
 
-		if offset := limit.Offset; offset > 0 {
+		if limit.Offset > 0 {
 			builder.WriteString(" OFFSET ")
-			builder.WriteString(strconv.Itoa(offset))
+			builder.WriteString(strconv.Itoa(limit.Offset))
 			builder.WriteString(" ROWS")
 		}
-		if limit := *limit.Limit; limit > 0 {
+
+		var lmt = -1
+		if limit.Limit != nil && *limit.Limit >= 0 {
+			lmt = *limit.Limit
+		}
+		if lmt >= 0 {
 			builder.WriteString(" FETCH NEXT ")
-			builder.WriteString(strconv.Itoa(limit))
+			builder.WriteString(strconv.Itoa(lmt))
 			builder.WriteString(" ROWS ONLY")
 		}
 	}
